db9dffb86852c5ea5588aa550765a29b038b71c5
Tho Dang <tho.anguc@ohmidasvn.com>
2024-10-04 17:21:21 +0700

    feat: add frame motion

---
+++ app/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import Header from "../Header";
+import MainSwiper from "../MainSwiper";
+import OurServices from "../OurServices";
+import Test from "../Test";
+
+export default function Main() {
+  return (
+    <div className="h-full w-full">
+      <Header />
+
+      <MainSwiper />
+
+      <OurServices />
+
+      <Test />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import Header from "./components/Header";
-import MainSwiper from "./components/MainSwiper";
-import OurServices from "./components/OurServices";
+import Main from "./components/Main";
 
 export default function Home() {
-  return (
-    <div className="h-full w-full">
-      <Header />
-
-      <MainSwiper />
-
-      <OurServices />
-    </div>
-  );
+  return <Main />;
 }
